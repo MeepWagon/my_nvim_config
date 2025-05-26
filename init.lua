6871464cfbb1b953e3de500236524ab01276e294
Angelus Hein <angelushein2004@gmail.com>
2025-05-26 09:03:45 -0500

    This file isnt finished yet, nothing of note to write

--- init.lua
+++ init.lua
@@ -1,27 +1,55 @@
 -- This is the code required for ~/.config/nvim/init.lua
--- to load all the nvim_config.lua files in a project
 
 
+-- require("plugins")
+
+-- Load all the nvim_config.lua files in a project
 local config_path = vim.fn.getcwd() 
 local local_config_path = "/config/nvim_config"
 
-local final_path = nil
-function ascend_filepath(path)
+local project_root_dir = nil
+function ascend_filepath(path, local_path)
     if vim.fn.filereadable(path.."/config/nvim_config.lua") == 0 then
         local start_index = path:find("/([^/]+)$")
-        local modified_path = string.sub(path, 1, start_index-1)
-        -- print(modified_path)
-        ascend_filepath(modified_path)
+        if start_index ~= nil then
+            local modified_path = string.sub(path, 1, start_index-1)
+            ascend_filepath(modified_path)
+        end
     elseif vim.fn.filereadable(path.."/config/nvim_config.lua") == 1 then
-        final_path = path
+        project_root_dir = path
         return
     end
 end
 
+-- Get project config file
 ascend_filepath(config_path)
-
-if final_path ~= nil then
-    dofile(final_path.."/config/nvim_config.lua")
+if project_root_dir ~= nil then
+    -- package.path = package.path..";"..project_root_dir.."/config/plugins/?.lua"
+    -- package.path = package.path..";"..project_root_dir.."/config/?.lua"
+    -- package.path = package.path..";"..project_root_dir.."/plugins/nvim-tree/?.lua"
+    if project_root_dir then
+        print(project_root_dir)
+        -- Add these paths in correct order
+        package.path = package.path .. ";" .. project_root_dir .. "/config/lua/?.lua"
+        package.path = package.path .. ";" .. project_root_dir .. "/config/lua/?/init.lua"
+        package.path = package.path .. ";" .. project_root_dir .. "/config/plugins/?/init.lua"
+        package.path = package.path .. ";" .. project_root_dir .. "/config/plugins/nvim-tree/?.lua"
+        package.path = package.path .. ";" .. project_root_dir .. "/config/plugins/nvim-tree/?/init.lua"
+        package.path = package.path..";"..project_root_dir.."/config/?.lua"
+        -- print(vim.fn.expand(project_root_dir.."/config/?.lua"))
+        -- print(project_root_dir.."/config/?.lua")
+        
+        -- require("nvim_config")
+    end
+    -- vim.opt.runtimepath:append("/home/angelus/Documents/rps_project/config")
+    -- vim.opt.runtimepath:append("/home/angelus/Documents/rps_project/config/plugins")
+    -- vim.opt.runtimepath:append("/home/angelus/Documents/rps_project/config/plugins/nvim-tree")
+    dofile(project_root_dir.."/config/nvim_config.lua")
+    -- require("nvim_config")
 else
     print("No nvim_config.lua found.")
 end
+
+-- print(package.path.."\n")
+
+vim.cmd("so")
